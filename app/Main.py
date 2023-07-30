6939a25b224cba5495357566620e59336a11cc8b
Srijan <john.rambo.9901@gmail.com>
2023-07-30 21:07:03 +0530

    Organized Code

---
+++ app/Main.py
@@ -0,0 +1,8 @@
+import lib.CustomFunctions as cf
+
+print("Hello , there My Name is SkySync AI how can I help you today ?? ")
+
+input("Please tell your city name ")
+
+cf.UserInput()
+
---
+++ lib/CustomFunctions.py
@@ -0,0 +1,4 @@
+def UserInput():
+    city_name = str(input("Enter city name: ")).lower()
+    return print(city_name)
+
