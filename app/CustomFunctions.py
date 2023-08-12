c94d725bb8f320d8d3ea0f03946ef2493e1fb36e
Srijan <john.rambo.9901@gmail.com>
2023-08-12 19:09:19 +0530

    Clear Goal Let's go

--- app/CustomFunctions.py
+++
@@ -1,7 +0,0 @@
-def UserInput():
-    city_name = str(input("Enter city name: ")).lower()
-    return city_name.capitalize()
-
-
-def JustWeather(take):
-    return
--- app/Main.py
+++
@@ -1,6 +0,0 @@
-import CustomFunctions as cf
-
-print("Hello , there My Name is SkySync AI how can I help you today ?? ")
-decision = str(input(""))
-take = cf.UserInput()
-print(take)
