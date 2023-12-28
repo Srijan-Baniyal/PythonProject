6022a5eb6d9b060ab07af681b7d3685f9cf5a3a7
Srijan <john.rambo.9901@gmail.com>
2023-12-28 18:33:27 +0530

    Done with the Project

--- CustomFunction.py
+++
@@ -1,7 +0,0 @@
-def options():
-    return int(input("Enter number of the option you want to choose: "))
-
-
-def UserInput():
-    city_name = input("Enter city name: ").capitalize()
-    return city_name
